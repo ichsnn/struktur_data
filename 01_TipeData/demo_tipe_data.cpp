4721747bac1bec3d6c7672ddbbf00fbc94d86832
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-04-23 10:40:17 +0700

    code

---
+++ 01_TipeData/demo_tipe_data.cpp
@@ -0,0 +1,27 @@
+//tipe data terstruktur
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    struct TMhs {
+        char nim[9];
+        char nama[30];
+        short uts, uas;
+        float nilai_akhir;        
+    };
+    TMhs mhs;
+    cout<<"NIM  : ";cin.getline(mhs.nim, sizeof(mhs.nim));
+    cout<<"Nama : ";cin.getline(mhs.nama, sizeof(mhs.nama));
+    cout<<"UTS  : ";cin>>mhs.uts;
+    cout<<"UAS  : ";cin>>mhs.uas;
+    mhs.nilai_akhir = 0.40 * mhs.uts + 0.60 * mhs.uas;
+    cout<<"Mahasiswa yang bernama "<<mhs.nama<<" mempunyai nilai "<<mhs.nilai_akhir<<endl;
+
+
+    return 0;
+
+}
