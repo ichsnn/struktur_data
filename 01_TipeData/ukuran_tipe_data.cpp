bb3dbb2fbe77755e91a78037c65cdbdda8cde43a
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-04-23 10:05:33 +0700

    code

--- 01_TipeData/ukuran_tipe_data.cpp
+++ 01_TipeData/ukuran_tipe_data.cpp
@@ -6,6 +6,13 @@ using namespace std;
 
 int main() {
 
+    cout<<"Tipe Karakter"<<endl;
+    cout<<"char                   : "<<sizeof(char)<<endl;
+    cout<<"char16_t               : "<<sizeof(char16_t)<<endl;
+    cout<<"char32_t               : "<<sizeof(char32_t)<<endl;
+    cout<<"wchar_t                : "<<sizeof(wchar_t)<<endl;
+    cout<<endl;
+
     cout<<"Tipe Integer (Signed)"<<endl;
     cout<<"signed char            : "<<sizeof(signed char)<<endl;
     cout<<"signed short int       : "<<sizeof(signed short int)<<endl;
@@ -26,7 +33,19 @@ int main() {
     cout<<"float                  : "<<sizeof(float)<<endl;
     cout<<"double                 : "<<sizeof(double)<<endl;
     cout<<"long double            : "<<sizeof(long double)<<endl;
-    
+    cout<<endl;
+
+    cout<<"Tipe Boolean"<<endl;
+    cout<<"bool                   : "<<sizeof(bool)<<endl;
+    cout<<endl;
+
+    cout<<"Tipe Void"<<endl;
+    cout<<"void                   : no storage"<<endl;
+    cout<<endl;
+
+    cout<<"Null pointer"<<endl;
+    cout<<"declytype(nullptr)     : "<<sizeof(decltype(nullptr))<<endl;
+
     return 0;
 
 }
