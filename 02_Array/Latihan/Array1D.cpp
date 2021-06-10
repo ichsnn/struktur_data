13bdbc9479007f21154fa8f5d5a871516c934ae8
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-06-10 14:30:08 +0700

    code

--- 02_Array/Latihan/Array1D.cpp
+++ 02_Array/Latihan/Array1D.cpp
@@ -10,8 +10,10 @@ typedef int LarikInt[NMAKS + 1];
 void bacaData(LarikInt &data1, int N);                              // 1. Baca Data
 float total(LarikInt data1, int N);                                 // 2. Tampilkan Total Penjumlahan Semua Elemen
 float rerata(LarikInt data1, int N);                                // 3. Tampilkan Rata-Rata
-void terkecil(LarikInt data1, int N, int &kecil, int &frek);        // 4. Tampilkan Terkecil dan Frekuensinya
-
+int frekuensi(LarikInt data1, int N);                               // Pencari frekuensi
+void terkecil(LarikInt data1, int N, int &min1, int &fmin1);        // 4. Tampilkan Nilai Terkecil dan Banyaknya    
+void terbesar(LarikInt data1, int N, int &max1, int &fmax1);        // 5. Tampilkan Nilai Terbesar dan Banyaknya
+                                      
 //PROGRAM UTAMA
 int main(int argc, char const *argv[]) {
 
@@ -34,10 +36,12 @@ int main(int argc, char const *argv[]) {
     sum = total(data, banyakData);
     u = rerata(data, banyakData);
     terkecil(data, banyakData, min, fmin);    
+    terbesar(data, banyakData, max, fmax);
 
     cout<<"Total            : "<<sum<<endl;                         //2. Tampilkan Total Penjumlahan Semua Elemen
     cout<<"Rata - Rata      : "<<u<<endl;                           //3. Tampilkan Rata-Rata
-    cout<<"Terkecil         : "<<min<<" sebanyak "<<fmin<<endl;     //4. Tampilkan Nilai Terkecil
+    cout<<"Terkecil         : "<<min<<" sebanyak "<<fmin<<endl;     //4. Tampilkan Nilai Terkecil dan Banyaknya
+    cout<<"Terbesar         : "<<max<<" sebanyak "<<fmax<<endl;     //5. Tampilkan Nilai Terbesar dan Banyaknya
 
     return 0;
 }
@@ -74,24 +78,43 @@ float rerata(LarikInt data1, int N) {
     //ALGORITMA
     return total(data1, N)/N;    
 }         
-
-void terkecil(LarikInt data1, int N, int &kecil, int &frek) {
-//4. Tampilkan Nilai Terkecil dan Banyaknya
+int frekuensi(LarikInt data1, int N, int a, int fa) {
+//Pencari Frekuensi
     //DEKLARASI
     int i;
-    LarikInt countMin;
+    LarikInt countData1;    
     //ALGORITMA
-    kecil = data1[1];
+    for(i=1;i<=fa;i++) {
+        countData1[i] = 0;
+    }
+    for(i=1;i<=N;i++) {
+        countData1[data1[i]] = countData1[data1[i]] + 1;
+    }    
+    return countData1[a];
+}
+void terkecil(LarikInt data1, int N, int &min1, int &fmin1) {
+//4. Tampilkan Nilai Terkecil dan Banyaknya
+    //DEKLARASI
+    int i;     
+    //ALGORITMA
+    min1 = data1[1];
     for(i=2;i<=N;i++) {
-        if(data1[i] < kecil) {
-            kecil = data1[i];
+        if(data1[i] < min1) {
+            min1 = data1[i];
         }
     }
-    for(i=1;i<=N;i++) {
-        countMin[i] = 0;
+    fmin1 = frekuensi(data1, N, min1, NMAKS);    
+}        
+void terbesar(LarikInt data1, int N, int &max1, int &fmax1) {
+//5. Tampilkan Nilai Terbesar dan Banyaknya    
+    //DEKLARASI
+    int i;
+    //ALGORITMA
+    max1 = data1[1];
+    for(i=2;i<=N;i++) {
+        if(data1[i] > max1) {
+            max1 = data1[i];
+        }
     }
-    for(i=1;i<=N;i++) {
-        countMin[data1[i]] = countMin[data1[i]] + 1;
-    }
-    frek = countMin[kecil];
-}
+    fmax1 = frekuensi(data1, N, max1, NMAKS);    
+}        
