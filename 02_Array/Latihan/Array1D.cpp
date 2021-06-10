316b3f0e48a4f2c09740e51827e8e56d83bf6b4c
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-06-10 13:56:17 +0700

    code

--- 02_Array/Latihan/Array1D.cpp
+++ 02_Array/Latihan/Array1D.cpp
@@ -7,9 +7,10 @@ const int NMAKS = 100;
 typedef int LarikInt[NMAKS + 1];
 
 //PURWARUPA PROCEDURE YANG AKAN DIGUNAKAN
-void bacaData(LarikInt &data1, int N);      // 1. Baca Data
-float total(LarikInt data1, int N);           // 2. Tampilkan Total Penjumlahan Semua Elemen
-float rerata(LarikInt data1, int N);          // 3. Tampilkan Rata-Rata
+void bacaData(LarikInt &data1, int N);                              // 1. Baca Data
+float total(LarikInt data1, int N);                                 // 2. Tampilkan Total Penjumlahan Semua Elemen
+float rerata(LarikInt data1, int N);                                // 3. Tampilkan Rata-Rata
+void terkecil(LarikInt data1, int N, int &kecil, int &frek);        // 4. Tampilkan Terkecil dan Frekuensinya
 
 //PROGRAM UTAMA
 int main(int argc, char const *argv[]) {
@@ -17,6 +18,11 @@ int main(int argc, char const *argv[]) {
     //DEKLARASI
     LarikInt data;
     int banyakData;
+    float sum;          //total data
+    float u;            //rata - rata
+    int min, fmin;      //nilai terkecil dan banyaknya
+    int max, fmax;      //nilai terbesar dan banyaknya
+
 
     //PROGRAM UTAMA    
     cout<<"Banyak Data : ";cin>>banyakData;
@@ -25,8 +31,13 @@ int main(int argc, char const *argv[]) {
 
     cout<<endl;
 
-    cout<<"Total            : "<<total(data, banyakData)<<endl;     //2. Tampilkan Total Penjumlahan Semua Elemen
-    cout<<"Rata - Rata      : "<<rerata(data, banyakData)<<endl;    //3. Tampilkan Rata-Rata
+    sum = total(data, banyakData);
+    u = rerata(data, banyakData);
+    terkecil(data, banyakData, min, fmin);    
+
+    cout<<"Total            : "<<sum<<endl;                         //2. Tampilkan Total Penjumlahan Semua Elemen
+    cout<<"Rata - Rata      : "<<u<<endl;                           //3. Tampilkan Rata-Rata
+    cout<<"Terkecil         : "<<min<<" sebanyak "<<fmin<<endl;     //4. Tampilkan Nilai Terkecil
 
     return 0;
 }
@@ -63,3 +74,24 @@ float rerata(LarikInt data1, int N) {
     //ALGORITMA
     return total(data1, N)/N;    
 }         
+
+void terkecil(LarikInt data1, int N, int &kecil, int &frek) {
+//4. Tampilkan Nilai Terkecil dan Banyaknya
+    //DEKLARASI
+    int i;
+    LarikInt countMin;
+    //ALGORITMA
+    kecil = data1[1];
+    for(i=2;i<=N;i++) {
+        if(data1[i] < kecil) {
+            kecil = data1[i];
+        }
+    }
+    for(i=1;i<=N;i++) {
+        countMin[i] = 0;
+    }
+    for(i=1;i<=N;i++) {
+        countMin[data1[i]] = countMin[data1[i]] + 1;
+    }
+    frek = countMin[kecil];
+}
