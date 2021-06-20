5bd7879f563bb087edd7fdc9eba96a7a3917bf1f
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-06-20 11:35:34 +0700

    code

--- 02_Array/Array2Dimensi.cpp
+++ 02_Array/Array2Dimensi.cpp
@@ -2,20 +2,82 @@
 
 using namespace std;
 
-const int BARIS = 100;  //BATAS MAKSIMAL BARIS YANG BISA DIISI
-const int KOLLOM = 100; //BATAS MAKSIMAL KOLOM YANG BISA DIISI
+const int MAKS = 100;
 
+typedef int tMatrik[MAKS][MAKS];
+
+//DEKLARASI PURWARUPA FUNGSI YANG AKAN DIGUNAKAN
+void tampilMatrik(tMatrik M, int b, int k);         
+void inisialisasiMatrik(tMatrik &M, int b, int k);
+
+
+//PROGRAM UTAMA
 int main(int argc, char const *argv[]) {
-    int matriks[BARIS][KOLLOM] = { 
-        {1, 2},
-        {2, 3},         //Baris 3,   Kolom 2
-        {3, 2},
-    };    
-    for(int i = 0; i<3; i++) {
-        for(int j=0; j<2; j++) {
-            cout<<matriks[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+
+    //DEKLARASI
+    int m1[MAKS][MAKS] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int i;
+    int j;
+    int baris, kolom;    
+
+    //ALGORITMA    
+    baris = 3;
+    kolom = 3; 
+        
+    tampilMatrik(m1, baris, kolom);
+    cout<<endl;
+
+    //setelah diinisialisasi
+    inisialisasiMatrik(m1, baris, kolom);
+    tampilMatrik(m1, baris, kolom);            
     return 0;
+}
+
+//FUNGSI YANGA AKAN DIGUNAKAN
+
+void tampilMatrik(tMatrik M, int b, int k) {
+//tampil matrik
+
+    //DEKLARASI
+    int i;
+    int j;        
+
+    //ALGORITMA
+    cout<<"Isi Matrik : \n"<<endl;
+    i = 0;
+    while(i<b) {
+        j=0;
+        while(j<k) {
+            cout<<M[i][j]<<"\t";
+            j++;
+        }        
+        cout<<endl;
+        i++;
+    }
+    cout<<endl;
+    cout<<"Tekan Enter Untuk Lanjut... ";   
+    getchar();         
+}
+
+void inisialisasiMatrik(tMatrik &M, int b, int k) {
+//inisialisasi matrik
+
+    //DEKLARASI
+    int i;
+    int j;
+
+    //ALGORITMA        
+    i = 0;
+    while(i<b) {
+        j=0;
+        while(j<k) {
+            M[i][j] = 0;
+            j++;
+        }                
+        i++;
+    }    
 }
