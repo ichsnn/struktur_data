9fbabf1ecdfc79eea96f64d68f2c852a8ad72ab4
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-06-21 15:57:32 +0700

    finale

--- 02_Array/Latihan/Array2D.cpp
+++ 02_Array/Latihan/Array2D.cpp
@@ -11,6 +11,7 @@ struct kuliah {
     int nim;
     int sks;    
     char nilai;
+    float ip;
 };
 
 typedef kuliah tabel[MAKS][MAKS];
@@ -33,13 +34,11 @@ int main(int argc, char const *argv[]) {
     int kolom;
 
     //ALGRORITMA    
-    cout<<"Banyak Mata Kuliah \t: ";cin>>banyak_matkul; 
-    cout<<"-------------------------------------------"<<endl;
+    cout<<"Banyak Mata Kuliah \t: ";cin>>banyak_matkul;     
     mataKuliah(data, banyak_matkul);
 
     cout<<endl;
-    cout<<"Banyak Mahasiswa \t: ";cin>>banyak_mhs; 
-    cout<<"-------------------------------------------"<<endl;
+    cout<<"Banyak Mahasiswa \t: ";cin>>banyak_mhs;     
     mahasiswa(data, banyak_mhs);
 
     baris = banyak_mhs;
@@ -93,27 +92,65 @@ void nilai(tabel &tbl, int b, int k) {
     //ALGORITMA    
     for(i=1;i<=b;i++) {
         for(j=1;j<=k;j++) {                                                    
-            cout<<"Nilai Mhs "<<tbl[i][1].nim<<" mata kuliah "<<tbl[1][j].nama_matkul<<" : ";cin>>tbl[i][j].nilai;            
+            cout<<"Nilai Mhs "<<tbl[i][1].nim<<" mata kuliah "<<tbl[1][j].nama_matkul<<"\t";
+            if(tbl[1][j].nama_matkul.size()<=8) cout<<"\t";
+            cout<<": ";cin>>tbl[i][j].nilai;            
         }
     }
 }
 void gambar(tabel tbl, int b, int k) {
 //tabel ouput    
     //DEKLARASI
-    int i;
-    int j;    
+    int i, j;  
+    int sum_sks = 0;             
+
+    //ALGORITMA
     for(i=1;i<=k;i++) {                     
         if(i==1) cout<<"\t\t";
-        cout<<tbl[1][i].nama_matkul<<"\t";             
+        cout<<tbl[1][i].nama_matkul<<"\t";                     
     }
+    cout<<"IP";
     cout<<endl;
-    cout<<"---------------------------------------------------------"<<endl;
+    cout<<"-----------------------------------------------------------"<<endl;
+    for(i=1;i<=k;i++) {
+        sum_sks += tbl[1][i].sks;        
+    }  
+
+    k++;
+    for(i=1;i<=b;i++) {         
+        tbl[i][k].ip = 0;
+        for(j=1;j<=k;j++) {
+            if(j<k) {
+                switch (tbl[i][j].nilai) {
+                case 'A':
+                    tbl[i][k].ip += 4.0 * tbl[1][j].sks;                            
+                    break;
+                case 'B':
+                    tbl[i][k].ip += 3.0 * tbl[1][j].sks;                            
+                    break;
+                case 'C':
+                    tbl[i][k].ip += 2.0 * tbl[1][j].sks;                            
+                    break;
+                case 'D':
+                    tbl[i][k].ip += 1.0 * tbl[1][j].sks;                            
+                    break;                            
+                default:
+                    tbl[i][k].ip += 0;                            
+                    break;
+                }
+            }           
+        }     
+        tbl[i][k].ip = (tbl[i][k].ip / sum_sks);  
+    }
     for(i=1;i<=b;i++) {         
         for(j=1;j<=k;j++) {
-            if(j==1) {
-                cout<<tbl[i][j].nim<<"\t";
-            }                       
-            cout<<tbl[i][j].nilai<<"        \t"; 
+            if(j<k) {
+                if(j==1) {
+                    cout<<tbl[i][j].nim<<"\t";
+                }                       
+                cout<<tbl[i][j].nilai<<"\t"; 
+                if(tbl[1][j].nama_matkul.size()>5) cout<<"\t";
+            } else cout<<tbl[i][j].ip;
         }        
         cout<<endl;               
     }
