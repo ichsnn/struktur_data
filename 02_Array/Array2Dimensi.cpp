89cfc3e751373ca39e8491eb465cfaec4ab8c4c1
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-06-21 08:50:03 +0700

    add penjumlahanMatrik function

--- 02_Array/Array2Dimensi.cpp
+++ 02_Array/Array2Dimensi.cpp
@@ -10,12 +10,12 @@ typedef int tMatrik[MAKS][MAKS];
 void tampilMatrik(tMatrik M, int b, int k);         
 void inisialisasiMatrik(tMatrik &M, int b, int k);
 void bacaMatrik(tMatrik &M, int b, int k);
-
+void penjumlahanMatrik(tMatrik M1, tMatrik M2, int b, int k, tMatrik &M3);
 //PROGRAM UTAMA
 int main(int argc, char const *argv[]) {
 
     //DEKLARASI
-    tMatrik m1;        
+    tMatrik m1, m2, m3;        
     int i;
     int j;
     int baris, kolom;    
@@ -25,15 +25,30 @@ int main(int argc, char const *argv[]) {
     kolom = 3; 
         
     //Matrik diinisialisasi    
+    system("cls");
+    cout<<"INISIALISASI MATRIK\n"<<endl;
     inisialisasiMatrik(m1, baris, kolom);
     tampilMatrik(m1, baris, kolom);            
     cout<<endl;
 
     //Baca Matrik    
+    system("cls");
+    cout<<"BACA MATRIK\n"<<endl;
     bacaMatrik(m1, baris, kolom);
     fflush(stdin);cout<<endl;              
-    tampilMatrik(m1, baris, kolom);            
+    tampilMatrik(m1, baris, kolom);                
 
+    //Penjumlahan Matrik
+    system("cls");
+    cout<<"PENJUMLAHAN MATRIKS\n"<<endl;
+    bacaMatrik(m1, baris, kolom);
+    fflush(stdin);cout<<endl;
+    bacaMatrik(m2, baris, kolom);
+    fflush(stdin);cout<<endl;
+    penjumlahanMatrik(m1, m2, baris, kolom, m3);
+    tampilMatrik(m3, baris, kolom);
+
+    system("cls");
     return 0;
 }
 
@@ -93,5 +108,17 @@ void bacaMatrik(tMatrik &M, int b, int k) {
         for(j=0; j<k; j++) {
             cout<<"Data Baris "<<i<<", Kolom "<<j<<" : ";cin>>M[i][j];
         }
+    }    
+}
+
+void penjumlahanMatrik(tMatrik M1, tMatrik M2, int b, int k, tMatrik &M3) {
+//penjumlahan matrik
+
+    //DEKLARASI MATRIK
+    int i, j;
+
+    //ALGORITMA    
+    for(i=0; i<b; i++) {
+        for(j=0; j<k; j++) M3[i][j] = M1[i][j] + M2[i][j];        
     }    
 }
