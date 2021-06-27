e3f4e133f9d4e561ffb4daccd474f7f948df7257
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-06-27 20:20:19 +0700

    stack array

--- 04_Stack/Array/StackArray.cpp
+++ 04_Stack/Array/StackArray.cpp
@@ -11,34 +11,99 @@ struct TStack {
 void init(TStack &stack);
 bool isEmpty(TStack stack);
 bool isFull(TStack stack);
+int count(TStack stack);
+void push(TStack &stack, int newElement);
+void pop(TStack &stack);
+void display(TStack stack);
+int peek(TStack stack);
+void empty(TStack &stack);
 //PROGRAM UTAMA
 int main() {
     //Deklarasi
     TStack stack;
     //Algoritma
     init(stack);    //inisialisasi
-    isEmpty(stack) ? cout<<"Kosong\n":cout<<"Isi\n";
+    display(stack);  //tampilkan stack
     cout<<endl;
-    stack.top++;
-    isFull(stack) ? cout<<"Penuh\n":cout<<"Tidak Penuh\n";
-    isEmpty(stack) ? cout<<"Kosong\n":cout<<"Isi\n";
-    stack.top = 100;
+    push(stack, 5);   //push        
+    push(stack, 7);
+    push(stack, 10);    
+    push(stack, 3);    
+    push(stack, 4);    
+    display(stack);  //tampilkan stack
     cout<<endl;
-    isEmpty(stack) ? cout<<"Kosong\n":cout<<"Isi\n";
-    isFull(stack) ? cout<<"Penuh\n":cout<<"Tidak Penuh\n";
-
+    pop(stack);
+    pop(stack);
+    pop(stack);
+    pop(stack);
+    pop(stack);
+    pop(stack);    
     return 0;
 }
 
-void init(TStack &stack) {       //nilai stack atau element dimulai dari index - 1
+void init(TStack &stack) {       
 //inisialisasi stack
-    stack.top = 0;
+    stack.top = 0; //nilai stack atau element dimulai dari index - 1
 }
 
 bool isEmpty(TStack stack) {
+//cek array kosong atau tidak    
     return stack.top == 0;
 }
 
 bool isFull(TStack stack) {
+//cek array penuh atau tidak    
     return stack.top == MAX_ELEMENT_STACK;
+}
+
+int count(TStack stack) {
+//hitung banyak elemen array    
+    return stack.top;
+}
+
+void push(TStack &stack, int newElement) {
+//tambah elemen stack
+    if(isFull(stack)) {
+        cout<<"ERROR : Data Penuh";
+    } else {
+        stack.top++;
+        stack.element[stack.top] = newElement;
+    }
+}
+
+void display(TStack stack) {
+//tampilkan elemen stack   
+    cout<<"Stack : "<<endl;
+    if(isEmpty(stack)) {
+        cout<<"[Data Kosong]"<<endl;
+    } else {
+        for(int i = stack.top; i >= 1; i-- ) {
+            cout<<stack.element[i];
+            if(i == stack.top) {
+                cout<<" <= TOP";
+            }
+            cout<<endl;
+        }
+    }        
+}
+
+void pop(TStack &stack) {
+//ambil data atau hapus stack
+    int data;
+    if(isEmpty(stack)) {
+        cout<<"ERROR : Data Kosong";              
+    } else {
+        data = stack.element[stack.top];
+        stack.top--;        
+        cout<<data;
+    }
+    cout<<endl;
+}
+int peek(TStack stack) {
+//melihat isi posisi terakhir stack atau top
+    return stack.element[stack.top];
+}
+void empty(TStack &stack) {
+//kosongkan stack
+    stack.top = 0;    
 }
